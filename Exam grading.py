a038ae6cc93ee4d69f24b7054cf5705121d8a81c
elmetodo <richardw@DI0328.DIPR.local>
2015-07-12 22:58:32 +0300

    attempt at assignment 3

---
+++ Exam grading.py
@@ -0,0 +1,31 @@
+def exam_model():
+
+# introduction
+
+	print ('Welcome to the grade caluclator for you English, Science and Maths exams')
+	print ('type you scores in and find out your grades')
+
+	name=raw_input ('What is you name: ')
+
+	english_score= input ('What was your exam score for English: ')
+	maths_score=input ('What was your exam score for Maths: ')
+	Science_score=input ('What was your exam score for Science: ')
+
+	for i in [english_score, maths_score, Science_score]
+
+		if >= 90
+		=grade_a
+
+		elif >= 80 and < 90
+		=grade_b
+		
+		# c_grade
+		# d_grade
+		# e_grade
+		# f_grade
+		# u_grade
+
+
+
+
+exam_model ()
